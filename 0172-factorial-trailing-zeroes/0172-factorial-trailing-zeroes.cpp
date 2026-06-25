4dd52e63b8c38733222f2d7de65ed852dfa10c99
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-06-25 21:46:53 +0530

    Enhance comments in trailingZeroes function
    Added comments explaining the use of Legendre's Theorem for calculating trailing zeroes in factorials.

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        //int f=1;
+        //This problem solved by using Lagendre's Theorem
+        //i.e Vp(n!)=∑[n/p^k] where k=1 to ∞ 
         int c=0;
         int i=5;
         while(i<=n)
         {
+            //Here for trailing zero we have to put p=5
+            //And keep adding till the condition become false
             c=c+(n/i);
+            //We require i to increas in 5's power.
             i=i*5;
         }
         return c;
