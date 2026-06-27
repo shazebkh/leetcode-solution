041d2fd46e8b5a8627ab20ce358d3ad36bf2df40
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-06-27 22:54:03 +0530

    Time: 4 ms (19.35%), Space: 95.7 MB (33.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -8,18 +8,16 @@ public:
         int m=nums1.size();
         int n=nums2.size();
         int h=(m+n)-1;
-        int l=0;
         int mid;
-        int k=l+h;
-            if((k+1)%2!=0)
+            if((h+1)%2!=0)
             {
-                mid=l+(h-l)/2;
+                mid=h/2;
                 int ans=result[mid];
                 return ans;
             }
             else
             {
-                mid=(l+(h-l)/2);
+                mid=h/2;
                 double ans=(result[mid+1]+result[mid])*0.5;
                 return ans;
             }
