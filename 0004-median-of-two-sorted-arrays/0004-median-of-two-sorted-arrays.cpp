717242dd1acddf4ff353d5a07056a08b16a3cba1
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-06-27 23:10:01 +0530

    Thought process
    Added thought process for future reference so that i can revise it easily without giving second thought

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,12 +4,15 @@ public:
         vector<int>result(nums1.size()+nums2.size());
         merge(nums1.begin(),nums1.end(),
             nums2.begin(),nums2.end(),
-            result.begin());//""
+            result.begin());
+        //"Key Idea:Resultant of merging the two sorted array will always be a sorted array"
+        //"Thou time complexity is O(m+n)"
         int m=nums1.size();
         int n=nums2.size();
         int h=(m+n)-1;
         int mid;
             if((h+1)%2!=0)
+                //Use "h+1" because I am searching the position not the index
             {
                 mid=h/2;
                 int ans=result[mid];
