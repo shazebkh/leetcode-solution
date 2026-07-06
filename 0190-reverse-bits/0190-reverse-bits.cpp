3c842bf2f8c49c585eeb1187859ef8078b7302d4
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-07-06 23:50:25 +0530

    Refactor reverseBits function for clarity
    all the instruction how i solved the problem

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        bitset<32> bit=n;
-        string s=bit.to_string();
+        bitset<32> bit=n;//Convert int and binary strings to bits
+        string s=bit.to_string();//saved it into string to reverse the bits 
         reverse(s.begin(),s.end());
-        bitset<32>bit1(s);
-        int rev=bit1.to_ulong();
+        bitset<32>bit1(s);//convert string into bits
+        int rev=bit1.to_ulong();//converted bits into integer
         return rev;
     }
 };
