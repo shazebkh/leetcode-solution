ec5577d96e1c26fa73f3ef6f0f23eb5ba8335875
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-07-17 21:00:49 +0530

    Optimize missing number calculation
    Refactor missing number algorithm to use sum formula.

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-      sort(nums.begin(),nums.end());
-      int i=0;
-      int num=0;
-      while(i<nums.size())
-      {
-        if(num==nums[i])
-        {
-            i++;
-            num++;
-        }
-        else
-        {
-            return num;
-        }
-
-      }
-      
-      return num;
+        int n=nums.size();
+        long int totalsum=(n*(n+1))/2;
+        long int sum=0;
+        for(int i=0;i<n;i++)
+            {
+                sum=sum+nums[i];
+            }
+        return totalsum-sum;
+        //It can be solve using XOR
+        //THE SUM OF WHOLE NUMBERS WILL ALWAYS BE A WHOLE NUMBER
     }
 };
