1c2f13f3a97adba8eeceb9e2459e1f5c04409d3f
shazeb <234477933+shazebkh@users.noreply.github.com>
2026-08-03 11:04:47 +0530

    Time: 0 ms (100%), Space: 8 MB (4.29%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if(n==0)
+        {
+            return 1;
+        }
+        bitset<32> bit(n);
+        string s=bit.to_string();
+        s.erase(0, s.find_first_not_of('0'));
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='0')
+            {
+                s[i]='1';
+            }
+            else
+            {
+                s[i]='0';
+            }
+        }
+        bitset<32> res(s);
+        long ans=res.to_ulong();
+        return ans;
+    }
+};
